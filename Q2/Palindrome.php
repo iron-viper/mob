b5f0f9bd4a5886888ece4f6985a4060410d45d8e
Aliaksandr Drabysheuski <adrabysheuski@itsupportme.com>
2018-03-02 15:41:50 +0300

    Q2

---
+++ Q2/Palindrome.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrabysheuski
+ * Date: 3/2/18
+ * Time: 3:17 PM
+ */
+
+class Palindrome
+{
+    /**
+     * @param string $string
+     *
+     * @return bool
+     * @throws \InvalidArgumentException
+     */
+    public function check(string $string): mixed
+    {
+        $string = strtolower($string);
+        $string = preg_replace('/[^\w]/', '', $string);
+
+        if (!$string) {
+            throw new \InvalidArgumentException("Wrong string was given.");
+        }
+
+        $revStr = strrev($string);
+
+        return ($string === $revStr);
+    }
+}
+
+// Realisation
+
+$palindrome = new Palindrome();
+
+$arrayFoCheck = [
+    "racecar",
+    "a man a plan a canal Panama.",
+    "R2D2",
+    "Desserts, I stressed!",
+    "Red rum, sir, is murder.",
+    "This is the final one",
+];
+
+try {
+    foreach ($arrayFoCheck as $string) {
+        echo $string . " => " . ($palindrome->check($string) ? "true" : "false") . PHP_EOL;
+    }
+} catch (\Exception $e) {
+    echo "Error:" . $e->getMessage();
+}
+
+
